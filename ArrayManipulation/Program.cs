0adeb82a57e40ecacd72952b6ec92692d422fb89
Vusal <Mustafayev.V@hotmail.com>
2019-07-25 23:04:31 +0400

    challenge is solved but terminated timeout is still

--- ArrayManipulation/Program.cs
+++ ArrayManipulation/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace ArrayManipulation
 {
@@ -9,13 +11,63 @@ namespace ArrayManipulation
         // Complete the arrayManipulation function below.
         static long arrayManipulation(int n, int[][] queries)
         {
+            var beg = DateTime.Now;
+            var a = 0;
+            var b = 0;
+            var k = 0;
+            var array = new long[n];
+            foreach (var t in queries)
+            {
+                a = t[0];
+                b = t[1];
+                k = t[2];
 
-            return 0;
+                for (var j = 1; j <= n; j++)
+                {
+                    if (j >= a && j <= b)
+                        array[j - 1] += k;
+                }
+            }
+            var maxValue = array.Max();
+            var end = DateTime.Now;
+            Console.WriteLine(end - beg);
+            return maxValue;
         }
 
+        static long arrayManipulation2(int n, int[][] queries)
+        {
+            var beg = DateTime.Now;
+            var a = 0;
+            var b = 0;
+            var k = 0;
+            var array = new List<long>();
+            for (var i = 0; i < n; i++)
+                array.Add(0);
+            foreach (var t in queries)
+            {
+                a = t[0];
+                b = t[1];
+                k = t[2];
+                for (var j = 1; j <= n; j++)
+                {
+                    if (t == queries[0])
+                        array.Add(0);
+
+                    if (j >= a && j <= b)
+                    {
+                        array[j - 1] += k;
+                    }
+                }
+            }
+
+            var maxValue = array.Max();
+            var end = DateTime.Now;
+            Console.WriteLine(end - beg);
+            return maxValue;
+        }
         static void Main(string[] args)
         {
-           // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
             string[] nm = Console.ReadLine().Split(' ');
 
             int n = Convert.ToInt32(nm[0]);
@@ -28,7 +80,7 @@ namespace ArrayManipulation
             {
                 queries[i] = Array.ConvertAll(Console.ReadLine().Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
             }
-            long result = arrayManipulation(n, queries);
+            long result = arrayManipulation2(n, queries);
             Console.WriteLine(result);
             Console.ReadLine();
             //textWriter.WriteLine(result);
